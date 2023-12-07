a8ef9346b9f55d0e818db8b0c03ee938a6b9cde2
Cameron Kelly <134543364+Camkelly16@users.noreply.github.com>
2023-12-06 18:33:33 -0600

    Add files via upload

---
+++ MyFirstProject/src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.print("Hello World \n");
+		// \n is to create a new line
+		//\t is to create a tab
+		
+		System.out.print("\tIt is hot outside");
+	}
+
+}
